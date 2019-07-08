42f4874806d9bc51318a29a7ef7c2aa0e0d77eca
Darin <darindev1000@gmail.com>
2019-07-08 15:34:03 -0700

    imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularFormLessonLive';
   accountType = ['Personal', 'Business'];
+  signupForm: FormGroup;
+
+  ngOnInit() {}
 }
